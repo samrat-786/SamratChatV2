994c51468fc7c7bb71c4706c5db89726b568227c
samrat-786 <sksamrat875@gmail.com>
2026-07-22 10:04:32 +0530

    Update MyAppWebViewClient.java

--- app/src/main/java/com/example/app/MyAppWebViewClient.java
+++ app/src/main/java/com/example/app/MyAppWebViewClient.java
@@ -1,15 +1,13 @@
 package com.example.app;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MyAppWebViewClient extends WebViewClient {
 
     @Override
-    @Override
 public boolean shouldOverrideUrlLoading(WebView view, String url) {
     view.loadUrl(url);
     return true;
+}
 }
