f272f2fd965c2063bef76a83f30b3f36159f7f0a
samrat-786 <yourgmail@gmail.com>
2026-07-25 09:24:16 +0530

    Show splash logo again

--- app/src/main/java/com/example/app/SplashActivity.java
+++ app/src/main/java/com/example/app/SplashActivity.java
@@ -1,7 +1,5 @@
 package com.example.app;
-import android.widget.ImageView;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -12,10 +10,6 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
-ImageView logo = findViewById(R.id.logo);
-
-Animation fade = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-logo.startAnimation(fade);
 
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
