a53fe28ff96c0f1362f4d237b6d9f32fc2f4e2df
samrat-786 <yourgmail@gmail.com>
2026-07-28 13:03:07 +0530

    Fix message time timezone

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let users = {};
+
+io.on("connection", (socket) => {
+
+  socket.on("join", (name) => {
+  socket.username = name.trim().toLowerCase();
+    users[name] = socket.id;
+
+    io.emit("userList", Object.keys(users));
+
+    socket.emit("private message", {
+      from: "System",
+      message: "আপনি লগইন করেছেন: " + name
+    });
+  });
+socket.on("chat message", (message) => {
+const msgId = Date.now();
+  io.emit("chat message", {
+  from: socket.username,
+  message: message,
+ time: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
+  id: msgId
+});
+});
+socket.on("image message", (data) => {
+  io.emit("image message", {
+    from: socket.username,
+    image: data.image
+  });
+});
+socket.on("voice message", (data) => {
+  io.emit("voice message", {
+    from: socket.username,
+    audio: data.audio
+  });
+});
+  socket.on("private message", (data) => {
+    const receiverId = users[data.to];
+
+    if (receiverId) {
+      io.to(receiverId).emit("private message", {
+        from: socket.username,
+        message: data.message
+      });
+
+      socket.emit("private message", {
+        from: "You",
+        message: data.message
+      });
+    }
+  });
+socket.on("message received", (data) => {
+  io.to(data.senderId).emit("message delivered", {
+    id: data.id
+  });
+});
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      delete users[socket.username];
+      io.emit("userList", Object.keys(users));
+    }
+  });
+
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
